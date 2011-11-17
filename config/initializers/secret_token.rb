2bb105b1b5fd52b7c4fc87dd480a19e9d4b3825a
Chris Johnson <chrisltd@gmail.com>
2011-11-17 13:02:12 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AdamsCompetition::Application.config.secret_token = '920ef225729b3de365ea081f99239914a90dd47e72fd2c4dbb8cd687813683fd10ead5e81bbb5c9d06954171f223a66d70738692f9107dd6b33001eeabbf2f2d'
