871a5c307714ef5a25ce4644eaa6a3ec5a251f95
ChrisLTD <chrisltd@gmail.com>
2011-12-12 15:49:38 -0500

    Added admin user, and hid page editing behind it

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,11 +2,15 @@ class PagesController < ApplicationController
   # GET /pages
   # GET /pages.json
   def index
-    @pages = Page.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @pages }
+  	if current_user.try(:admin?)
+			@pages = Page.all
+	
+			respond_to do |format|
+				format.html # index.html.erb
+				format.json { render json: @pages }
+			end
+    else
+    	redirect_to :permission_error
     end
   end
   
@@ -39,60 +43,80 @@ class PagesController < ApplicationController
   # GET /pages/new
   # GET /pages/new.json
   def new
-    @page = Page.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @page }
+  	if current_user.try(:admin?)
+			@page = Page.new
+	
+			respond_to do |format|
+				format.html # new.html.erb
+				format.json { render json: @page }
+			end
+		else
+    	redirect_to :permission_error
     end
   end
 
   # GET /pages/1/edit
   def edit
-    @page = Page.find(params[:id])
+  	if current_user.try(:admin?)
+   		@page = Page.find(params[:id])
+		else
+    	redirect_to :permission_error
+    end
   end
 
   # POST /pages
   # POST /pages.json
   def create
-    @page = Page.new(params[:page])
-
-    respond_to do |format|
-      if @page.save
-        format.html { redirect_to @page, notice: 'Page was successfully created.' }
-        format.json { render json: @page, status: :created, location: @page }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @page.errors, status: :unprocessable_entity }
-      end
+  	if current_user.try(:admin?)
+			@page = Page.new(params[:page])
+	
+			respond_to do |format|
+				if @page.save
+					format.html { redirect_to @page, notice: 'Page was successfully created.' }
+					format.json { render json: @page, status: :created, location: @page }
+				else
+					format.html { render action: "new" }
+					format.json { render json: @page.errors, status: :unprocessable_entity }
+				end
+			end
+		else
+    	redirect_to :permission_error
     end
   end
 
   # PUT /pages/1
   # PUT /pages/1.json
   def update
-    @page = Page.find(params[:id])
-
-    respond_to do |format|
-      if @page.update_attributes(params[:page])
-        format.html { redirect_to @page, notice: 'Page was successfully updated.' }
-        format.json { head :ok }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @page.errors, status: :unprocessable_entity }
-      end
+		if current_user.try(:admin?)
+			@page = Page.find(params[:id])
+	
+			respond_to do |format|
+				if @page.update_attributes(params[:page])
+					format.html { redirect_to @page, notice: 'Page was successfully updated.' }
+					format.json { head :ok }
+				else
+					format.html { render action: "edit" }
+					format.json { render json: @page.errors, status: :unprocessable_entity }
+				end
+			end
+		else
+    	redirect_to :permission_error
     end
   end
 
   # DELETE /pages/1
   # DELETE /pages/1.json
   def destroy
-    @page = Page.find(params[:id])
-    @page.destroy
-
-    respond_to do |format|
-      format.html { redirect_to pages_url }
-      format.json { head :ok }
+		if current_user.try(:admin?)
+			@page = Page.find(params[:id])
+			@page.destroy
+	
+			respond_to do |format|
+				format.html { redirect_to pages_url }
+				format.json { head :ok }
+			end
+		else
+    	redirect_to :permission_error
     end
   end
 end
